3ce9c7dd18186cb7f2ffb796042ea5a9046e8ad6
Diego Vottero Roca <diegovotteroroca@gmail.com>
2021-12-27 00:19:31 -0300

    formulario

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    isInicio: true
+  });
+});
+
+/* funcionamiento del formulario */
+router.post('/', async function (req,res,next){
+  /* console.log(req.body); */
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var comentarios = req.body.comentarios;
+
+  var obj ={
+    to: 'diegovotteroroca@gmail.com',
+    subject: 'Contacto desde la web de transportes',
+    html: nombre + ' se contacto a traves de la web y quiere saber mas info a este correo: ' + email + '.<br> y su Comentario es: ' + mensaje + ' .'
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST, //se comunique con .env
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+
+  });
+
+  var info = await transport.sendMail(obj); //envio de los datos
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+    /* isIndex: true; */
+  })
+
 });
 
 module.exports = router;
